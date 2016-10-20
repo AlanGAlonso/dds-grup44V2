a1eab9a12c9593048ad2e771ac7ade5618e6b9fc
MicaFontan <mfontan@intranet.certant.com>
2016-10-20 14:59:42 -0300

    Mepa que rompia lo anterior :O por las dudas lo comento

--- TPDDSGrupo44/DataModels/JsonCGP.cs
+++ TPDDSGrupo44/DataModels/JsonCGP.cs
@@ -8,6 +8,6 @@ namespace TPDDSGrupo44.DataModels
         public int comuna { get; set; }
         //public List<ServicioCGP> serviciosJSON { get; set; }
         //public object servicios { get; set; }
-        public List<ServiciosJSON> serviciosJson { get; set; }
+        //public List<ServiciosJSON> serviciosJson { get; set; }
     }
 }
