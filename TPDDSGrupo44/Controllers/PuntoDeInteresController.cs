14dc41578f74d298568764a056783705503c97a6
pyritewolf <jmlanghe@gmail.com>
2016-10-29 10:18:23 -0300

    FIX: Búsqueda de múltiples parámetros

--- TPDDSGrupo44/Controllers/PuntoDeInteresController.cs
+++ TPDDSGrupo44/Controllers/PuntoDeInteresController.cs
@@ -11,14 +11,7 @@ namespace TPDDSGrupo44.Controllers
     public class PuntoDeInteresController : ApiController
     {
         private BuscAR db = new BuscAR();
-
-        //// GET: api/PuntoDeInteres
-        //public IQueryable<PuntoDeInteres> GetPuntoDeInteres()
-        //{
-        //    return db.PuntoDeInteres;
-        //}
-
-        // GET: api/PuntoDeInteres/5
+        
         [HttpGet]
         [ResponseType(typeof(List<PuntoDeInteres>))]
         public IHttpActionResult GetPuntoDeInteres(string id)
@@ -26,12 +19,8 @@ namespace TPDDSGrupo44.Controllers
             SearchViewModel modelo;
             List<PuntoDeInteres> puntos = new List<PuntoDeInteres>();
 
-            if (id.Contains(","))
-            {
-                List<string> palabrasClave = id.Split(new char[] { ',' }).ToList();
-                foreach (string p in palabrasClave)
-                {
-                    modelo = MotorDeBusqueda.buscar(p);
+            
+                    modelo = MotorDeBusqueda.buscar(id);
                     puntos.AddRange(modelo.bancosEncontrados);
                     puntos.AddRange(modelo.bancosEncontradosCerca);
                     puntos.AddRange(modelo.cgpsEncontrados);
@@ -40,23 +29,7 @@ namespace TPDDSGrupo44.Controllers
                     puntos.AddRange(modelo.localesEncontradosCerca);
                     puntos.AddRange(modelo.paradasEncontradas);
                     puntos.AddRange(modelo.paradasEncontradasCerca);
-                }
-            } else
-            {
-                modelo = MotorDeBusqueda.buscar(id);
-                if (modelo.resultados == 0)
-                {
-                    return NotFound();
-                }
-                puntos.AddRange(modelo.bancosEncontrados);
-                puntos.AddRange(modelo.bancosEncontradosCerca);
-                puntos.AddRange(modelo.cgpsEncontrados);
-                puntos.AddRange(modelo.cgpsEncontradosCerca);
-                puntos.AddRange(modelo.localesEncontrados);
-                puntos.AddRange(modelo.localesEncontradosCerca);
-                puntos.AddRange(modelo.paradasEncontradas);
-                puntos.AddRange(modelo.paradasEncontradasCerca);
-            }
+             
 
             return Ok(puntos);
         }
--- TPDDSGrupo44/Models/MotorDeBusqueda.cs
+++ TPDDSGrupo44/Models/MotorDeBusqueda.cs
@@ -24,125 +24,21 @@ namespace TPDDSGrupo44.Models
 
                 //Defino ubicación actual (UTN/CAMPUS)
                 DispositivoTactil dispositivoTactil = db.Terminales.Include("funcionalidades").Where(i => i.nombre == "UTN FRBA Lugano").Single();
-                List<FuncionalidadDispositivoTactil> funcionalidadesTerminal = dispositivoTactil.funcionalidades.ToList();
                 
-                if (funcionalidadesTerminal.Any(f => f.nombre == "Parada")) { 
-                //Si la persona ingresó un número, asumo que busca una parada de bondi
-                    
-                        List<ParadaDeColectivo> resultadosBusqueda = db.Paradas.Include("palabrasClave").Include("horarioAbierto").Include("horarioFeriado").Where(b => b.palabrasClave.Where(p => p.palabraClave.ToLower().Contains(palabraBusqueda.ToLower())).Count() != 0).ToList();
-                        foreach (ParadaDeColectivo punto in resultadosBusqueda)
-                        {
-
-                            if (punto.estaCerca(dispositivoTactil.coordenada))
-                            {
-                                modeloVista.paradasEncontradasCerca.Add(punto);
-                            }
-                            else
-                            {
-                                modeloVista.paradasEncontradas.Add(punto);
-                            }
-
-                        }
-
-                }
-
-                if (funcionalidadesTerminal.Any(f => f.nombre == "Locales"))
+                List<string> palabrasClave = palabraBusqueda.Split(new char[] { ',' }).ToList();
+                foreach (string p in palabrasClave)
                 {
-                    //Si la persona ingresó una palabra, me fijo si es un rubro
-                    if (db.Rubros.Where(b => b.nombre.Contains(palabraBusqueda.ToLower())).ToList().Count() > 0)
-                    {
-
-                        List<LocalComercial> resultadosBusqueda = db.Locales.Include("horarioAbierto").Include("horarioFeriado").Where(b => b.rubro.nombre.ToLower().Contains(palabraBusqueda.ToLower())).ToList();
-                        foreach (LocalComercial punto in resultadosBusqueda)
-                        {
-                            if (punto.estaCerca(dispositivoTactil.coordenada))
-                            {
-                                modeloVista.localesEncontradosCerca.Add(punto);
-                            }
-                            else
-                            {
-                                modeloVista.localesEncontrados.Add(punto);
-                            }
-                        }
-
-                        // Si la palabra ingresada no era parada ni rubro, la busco como local
-                    }
-
-                    List<LocalComercial> resultadosBusquedaLocales = db.Locales.Include("horarioAbierto").Include("horarioFeriado").Include("rubro").Include("palabrasClave").Where(b => b.palabrasClave.Where(p => p.palabraClave.ToLower().Contains(palabraBusqueda.ToLower())).Count() != 0).ToList();
-                    if (resultadosBusquedaLocales.Count() > 0)
-                    {
-                        foreach (LocalComercial punto in resultadosBusquedaLocales)
-                        {
-                            if (!modeloVista.localesEncontrados.Any(l => l.id == punto.id)
-                            && !modeloVista.localesEncontradosCerca.Any(l => l.id == punto.id)) { 
-                                if (punto.estaCerca(dispositivoTactil.coordenada))
-                                {
-                                    modeloVista.localesEncontradosCerca.Add(punto);
-                                }
-                                else
-                                {
-                                    modeloVista.localesEncontrados.Add(punto);
-                                }
-                            }
-                        }
-                    }
+                    modeloVista = buscarParada(modeloVista, palabraBusqueda, dispositivoTactil, db);
+                    modeloVista = buscarLocal(modeloVista, palabraBusqueda, dispositivoTactil, db);
+                    modeloVista = buscarBanco(modeloVista, palabraBusqueda, dispositivoTactil, db);
+                    modeloVista = buscarCGP(modeloVista, palabraBusqueda, dispositivoTactil, db);
                 }
-
-                if (funcionalidadesTerminal.Any(f => f.nombre == "Banco"))
-                {
-                    List<Banco> resultadosBusquedaBancos = db.Bancos
-                        .Include("horarioAbierto").Include("horarioFeriado").Include("servicios").Include("servicios.horarioAbierto").Include("servicios.horarioFeriados").Include("palabrasClave")
-                        .Where(b => b.palabrasClave.Where(p => p.palabraClave.ToLower().Contains(palabraBusqueda.ToLower())).Count() != 0).ToList();
-
-
-                    List<Banco> resultadoBusquedaJSONBancos = GetJsonBanks.getJsonData().FindAll(b => b.palabrasClave.Where(p => p.palabraClave.ToLower().Contains(palabraBusqueda.ToLower())).ToList().Count() > 0);
-                    resultadosBusquedaBancos.AddRange(resultadoBusquedaJSONBancos);
-
-                    if (resultadosBusquedaBancos.Count() > 0)
-                    {
-                        foreach (Banco punto in resultadosBusquedaBancos)
-                        {
-                            if (punto.estaCerca(dispositivoTactil.coordenada))
-                            {
-                                modeloVista.bancosEncontradosCerca.Add(punto);
-                            }
-                            else
-                            {
-                                modeloVista.bancosEncontrados.Add(punto);
-                            }
-                        }
-                    }
-                }
-
-                if (funcionalidadesTerminal.Any(f => f.nombre == "CGP"))
-                {
-                    List<CGP> resultadosBusquedaCGP = db.CGPs.Include("horarioAbierto").Include("horarioFeriado").Include("servicios").Include("servicios.horarioAbierto").Include("servicios.horarioFeriados").Include("palabrasClave").Where(b => b.palabrasClave.Where(p => p.palabraClave.ToLower().Contains(palabraBusqueda.ToLower())).Count() != 0).ToList();
-
-                    List<CGP> resultadoBusquedaJSONCGP = GetJsonCGP.getJsonData().FindAll(c => c.palabrasClave.Where(p => p.palabraClave.ToLower().Contains(palabraBusqueda.ToLower())).ToList().Count() > 0);
-                    resultadosBusquedaCGP.AddRange(resultadoBusquedaJSONCGP);
-
-                    if (resultadosBusquedaCGP.Count() > 0)
-                    {
-                        foreach (CGP punto in resultadosBusquedaCGP)
-                        {
-                            if (punto.estaCerca(dispositivoTactil.coordenada))
-                            {
-                                modeloVista.cgpsEncontradosCerca.Add(punto);
-                            }
-                            else
-                            {
-                                modeloVista.cgpsEncontrados.Add(punto);
-                            }
-                        }
-                    }
-                }
-
 
                 contador.Stop();
 
                 modeloVista.resultados = modeloVista.bancosEncontrados.Count() + modeloVista.bancosEncontradosCerca.Count() + modeloVista.cgpsEncontrados.Count() + modeloVista.localesEncontrados.Count() + modeloVista.localesEncontradosCerca.Count() + modeloVista.paradasEncontradas.Count() + modeloVista.paradasEncontradasCerca.Count();
-                
-                if (dispositivoTactil.funcionalidades.Where(f => f.nombre == "Loggear Búsquedas").ToList().Count() != 0) { 
+
+                if (dispositivoTactil.funcionalidades.Where(f => f.nombre == "Loggear Búsquedas").ToList().Count() != 0) {
                     Busqueda busqueda = new Busqueda(palabraBusqueda, modeloVista.resultados, DateTime.Today, dispositivoTactil, contador.Elapsed);
                     db.Busquedas.Add(busqueda);
                     db.SaveChanges();
@@ -159,5 +55,150 @@ namespace TPDDSGrupo44.Models
             }
         }
 
+
+
+
+
+
+        private static SearchViewModel buscarParada(SearchViewModel modeloVista, string palabraBusqueda, 
+            DispositivoTactil dispositivoTactil, BuscAR db) {
+
+            List<FuncionalidadDispositivoTactil> funcionalidadesTerminal = dispositivoTactil.funcionalidades.ToList();
+            if (funcionalidadesTerminal.Any(f => f.nombre == "Parada"))
+            {
+                //Si la persona ingresó un número, asumo que busca una parada de bondi
+
+                List<ParadaDeColectivo> resultadosBusqueda = db.Paradas.Include("palabrasClave").Include("horarioAbierto").Include("horarioFeriado").Where(b => b.palabrasClave.Where(p => p.palabraClave.ToLower().Contains(palabraBusqueda.ToLower())).Count() != 0).ToList();
+                foreach (ParadaDeColectivo punto in resultadosBusqueda)
+                {
+
+                    if (punto.estaCerca(dispositivoTactil.coordenada))
+                    {
+                        modeloVista.paradasEncontradasCerca.Add(punto);
+                    }
+                    else
+                    {
+                        modeloVista.paradasEncontradas.Add(punto);
+                    }
+
+                }
+
+            }
+            return modeloVista;
+        }
+
+        private static SearchViewModel buscarLocal(SearchViewModel modeloVista, string palabraBusqueda, 
+            DispositivoTactil dispositivoTactil, BuscAR db)
+        {
+            List<FuncionalidadDispositivoTactil> funcionalidadesTerminal = dispositivoTactil.funcionalidades.ToList();
+            if (funcionalidadesTerminal.Any(f => f.nombre == "Locales"))
+            {
+                //Si la persona ingresó una palabra, me fijo si es un rubro
+                if (db.Rubros.Where(b => b.nombre.Contains(palabraBusqueda.ToLower())).ToList().Count() > 0)
+                {
+
+                    List<LocalComercial> resultadosBusqueda = db.Locales.Include("horarioAbierto").Include("horarioFeriado").Where(b => b.rubro.nombre.ToLower().Contains(palabraBusqueda.ToLower())).ToList();
+                    foreach (LocalComercial punto in resultadosBusqueda)
+                    {
+                        if (punto.estaCerca(dispositivoTactil.coordenada))
+                        {
+                            modeloVista.localesEncontradosCerca.Add(punto);
+                        }
+                        else
+                        {
+                            modeloVista.localesEncontrados.Add(punto);
+                        }
+                    }
+
+                    // Si la palabra ingresada no era parada ni rubro, la busco como local
+                }
+
+                List<LocalComercial> resultadosBusquedaLocales = db.Locales.Include("horarioAbierto").Include("horarioFeriado").Include("rubro").Include("palabrasClave").Where(b => b.palabrasClave.Where(p => p.palabraClave.ToLower().Contains(palabraBusqueda.ToLower())).Count() != 0).ToList();
+                if (resultadosBusquedaLocales.Count() > 0)
+                {
+                    foreach (LocalComercial punto in resultadosBusquedaLocales)
+                    {
+                        if (!modeloVista.localesEncontrados.Any(l => l.id == punto.id)
+                        && !modeloVista.localesEncontradosCerca.Any(l => l.id == punto.id))
+                        {
+                            if (punto.estaCerca(dispositivoTactil.coordenada))
+                            {
+                                modeloVista.localesEncontradosCerca.Add(punto);
+                            }
+                            else
+                            {
+                                modeloVista.localesEncontrados.Add(punto);
+                            }
+                        }
+                    }
+                }
+            }
+            return modeloVista;
+        }
+
+        private static SearchViewModel buscarBanco(SearchViewModel modeloVista, string palabraBusqueda, 
+            DispositivoTactil dispositivoTactil, BuscAR db)
+        {
+            List<FuncionalidadDispositivoTactil> funcionalidadesTerminal = dispositivoTactil.funcionalidades.ToList();
+            if (funcionalidadesTerminal.Any(f => f.nombre == "Banco"))
+            {
+                List<Banco> resultadosBusquedaBancos = db.Bancos
+                    .Include("horarioAbierto").Include("horarioFeriado").Include("servicios").Include("servicios.horarioAbierto").Include("servicios.horarioFeriados").Include("palabrasClave")
+                    .Where(b => b.palabrasClave.Where(p => p.palabraClave.ToLower().Contains(palabraBusqueda.ToLower())).Count() != 0).ToList();
+
+
+                List<Banco> resultadoBusquedaJSONBancos = GetJsonBanks.getJsonData().FindAll(b => b.palabrasClave.Where(p => p.palabraClave.ToLower().Contains(palabraBusqueda.ToLower())).ToList().Count() > 0);
+                resultadosBusquedaBancos.AddRange(resultadoBusquedaJSONBancos);
+
+                if (resultadosBusquedaBancos.Count() > 0)
+                {
+                    foreach (Banco punto in resultadosBusquedaBancos)
+                    {
+                        if (punto.estaCerca(dispositivoTactil.coordenada))
+                        {
+                            modeloVista.bancosEncontradosCerca.Add(punto);
+                        }
+                        else
+                        {
+                            modeloVista.bancosEncontrados.Add(punto);
+                        }
+                    }
+                }
+            }
+            return modeloVista;
+        }
+
+        private static SearchViewModel buscarCGP(SearchViewModel modeloVista, string palabraBusqueda, 
+            DispositivoTactil dispositivoTactil, BuscAR db)
+        {
+            List<FuncionalidadDispositivoTactil> funcionalidadesTerminal = dispositivoTactil.funcionalidades.ToList();
+            if (funcionalidadesTerminal.Any(f => f.nombre == "CGP"))
+            {
+                List<CGP> resultadosBusquedaCGP = db.CGPs.Include("horarioAbierto").Include("horarioFeriado").Include("servicios").Include("servicios.horarioAbierto").Include("servicios.horarioFeriados").Include("palabrasClave").Where(b => b.palabrasClave.Where(p => p.palabraClave.ToLower().Contains(palabraBusqueda.ToLower())).Count() != 0).ToList();
+
+                List<CGP> resultadoBusquedaJSONCGP = GetJsonCGP.getJsonData().FindAll(c => c.palabrasClave.Where(p => p.palabraClave.ToLower().Contains(palabraBusqueda.ToLower())).ToList().Count() > 0);
+                resultadosBusquedaCGP.AddRange(resultadoBusquedaJSONCGP);
+
+                if (resultadosBusquedaCGP.Count() > 0)
+                {
+                    foreach (CGP punto in resultadosBusquedaCGP)
+                    {
+                        if (punto.estaCerca(dispositivoTactil.coordenada))
+                        {
+                            modeloVista.cgpsEncontradosCerca.Add(punto);
+                        }
+                        else
+                        {
+                            modeloVista.cgpsEncontrados.Add(punto);
+                        }
+                    }
+                }
+            }
+            return modeloVista;
+        }
+
+
     }
+
+    
 }
